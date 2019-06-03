7bcf04a7b2ebdb6b3b46b58a1516f22e46302fd1
copperface <copperface08091993@gmail.com>
2019-06-03 22:09:34 +0500

    Updates

---
+++ precache-manifest.eebbc584bbc40bc56931232acff7a12a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c324bd69b26c98d1a720323b53b384f8",
+    "url": "/film_note/index.html"
+  },
+  {
+    "revision": "70e1a1609e35d41addf9",
+    "url": "/film_note/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "e9911e795d7c5f584b3d",
+    "url": "/film_note/static/js/2.576496b6.chunk.js"
+  },
+  {
+    "revision": "70e1a1609e35d41addf9",
+    "url": "/film_note/static/js/main.de3e3b84.chunk.js"
+  },
+  {
+    "revision": "2d2f12ba715b5b05fa93",
+    "url": "/film_note/static/js/runtime~main.6fe2181c.js"
+  }
+]);
